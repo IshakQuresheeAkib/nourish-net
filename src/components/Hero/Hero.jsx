2255bbede20e13203548f8d6be9db2df1771d0d4
IshaqQuresheeAkib <akibqureshee@gmail.com>
2023-11-05 09:40:14 +0600

    created login system

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ import Navbar from "../Navbar/Navbar";
 const Hero = () => {
     return (
         <div className="">                       
-            <div className="bg-[url('https://i.ibb.co/TtcLsG6/b.jpg')] w-screen h-screen text-white bg-cover bg-no-repeat">
-            <div className=" bg-gradient-to-b b from-black/90 w-full h-screen z-0 backdrop-blur-sm">
+            <div className="bg-[url('https://i.ibb.co/TtcLsG6/b.jpg')] w-screen h-screen text-white bg-cover bg-no-repeat bg-fixed">
+            <div className=" bg-gradient-to-b b from-black/90 w-full h-screen z-0 backdrop-blur-sm bg-fixed">
             <Navbar></Navbar>
             <div className="absolute bottom-16 tracking-wider translate-left-1/2 lg:left-10 right-0 text-center px-5">
                 <h1 className="lg:text-6xl text-4xl font-semibold mb-5 myHeading w-fit mx-auto text-white ">Linking Surplus to Need: <p className="textStyle font-bold"> Less Waste, More Food, </p> Stronger Community!</h1>
